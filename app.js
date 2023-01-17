35037b4d266afcac850df50048820e20ac5a3793
uurtech <girisiyorum@gmail.com>
2023-01-17 18:05:15 +0300

    progessiiinnggg

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
+
+require('dotenv').config()
+
+if (!process.env.GITHUB_TOKEN) {
+    console.log("please provide github token")
+    return 1
+}
+let rawdata = fs.readFileSync('./friends.json');
+let friends_list = JSON.parse(rawdata);
+
 const octokit = new Octokit({
-    auth: 'YOUR-TOKEN'
+    auth: process.env.GITHUB_TOKEN
   })
-  
-  await octokit.request('GET /users/{username}/repos{?type,sort,direction,per_page,page}', {
+
+
+friends_list.forEach(element => {
+    
+  octokit.request('GET /users/'+element+'/repos{?type,sort,direction,per_page,page}', {
     username: 'USERNAME'
   })
+});
+
+  
